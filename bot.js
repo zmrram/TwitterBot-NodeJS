8b688e2bdad5ff1d172618cf2251b5e2bdafe817
Tin Huynh <Tin Huynh>
2017-05-22 22:18:35 -0700

    authemticated dev

--- bot.js
+++ bot.js
@@ -1,8 +1,3 @@
 var Twit = require('twit')
 var twitAcess = require('./twitteraccess');
-var T = new Twit({
-    consumer_key: twitAcess.consumer_key,
-    consumer_secret: twitAcess.consumer_secret,
-    access_token: twitAcess.access_token,
-    access_token_secret: twitAcess.access_token_secret
-});
+var T = new Twit(twitAcess);
