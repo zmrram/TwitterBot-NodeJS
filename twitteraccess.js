9a761fa6cf8d8b24db2761237be2660256ad9c9e
Tin Huynh <tinhuynh@cpp.edu>
2017-05-23 17:56:06 -0700

    Update twitteraccess.js

--- twitteraccess.js
+++ twitteraccess.js
@@ -1,6 +1,19 @@
 module.exports = {
-    consumer_key: '',
-    consumer_secret: '',
-    access_token: '',
-    access_token_secret: ''
+    tweetterKey: {
+        consumer_key: '',
+        consumer_secret: '',
+        access_token: '',
+        access_token_secret: ''
+    },
+    wordNikKey: {
+        username: '',
+        password: '',
+        userSignature: "",
+        userId: '',
+        api_key: ''
+    },
+    google_API: {
+        cse_id: '',
+        api_key: ''
+    }
 };
